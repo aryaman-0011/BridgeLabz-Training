7f453a7d73beacf97ed3998515a76395b8bebd1a
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-12-16 16:43:33 +0530

    [Aryaman]: Added Celsius To Fahrenheit Conversion program

---
+++ gcr-codebase/programming-prerequisites/CelsiusToFahrenheitConversion.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CelsiusToFahrenheitConversion {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter temperature in celsius: ");
+		int celsius = sc.nextInt();
+
+		int fahrenheit = (celsius * 9/5) + 32;
+
+		System.out.println("Temperature in fahrenheit is: " + fahrenheit);
+	}
+}
