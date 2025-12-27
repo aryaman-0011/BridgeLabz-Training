b58980a67145a59b529766aa36b6613f4ddc3d57
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-12-27 20:45:42 +0530

    [Aryaman]: added Book Library System program

--- java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/constructorsandaccessmodifiers/level1/Books.java
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/constructorsandaccessmodifiers/level1/Books.java
@@ -1,5 +1,26 @@
 package com.constructorsandaccessmodifiers.level1;
 
-public class Books {
+class Books {
 
+    // Access modifiers
+    public String ISBN;        
+    protected String title;   
+    private String author;     
+
+    // Constructor
+    Books(String ISBN, String title, String author) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+    // Setter for private author
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Getter for private author
+    public String getAuthor() {
+        return author;
+    }
 }
