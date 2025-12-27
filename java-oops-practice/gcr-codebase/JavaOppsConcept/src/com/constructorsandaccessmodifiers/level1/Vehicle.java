b93ce26a9bac1baa7d7d37201aa6c440cd8001f9
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-12-27 19:32:47 +0530

    [Aryaman]: added Vehicle Registration program

--- java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/constructorsandaccessmodifiers/level1/Vehicle.java
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/constructorsandaccessmodifiers/level1/Vehicle.java
@@ -1,5 +1,48 @@
 package com.constructorsandaccessmodifiers.level1;
 
-public class Vehicle {
+class Vehicle {
 
+    // Instance variables
+    String ownerName;
+    String vehicleType;
+
+    // Class variable (shared by all vehicles)
+    static double registrationFee = 5000.0;
+
+    // Constructor
+    Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    // Instance method
+    void displayVehicleDetails() {
+        System.out.println(
+            "Owner Name: " + ownerName +
+            ", Vehicle Type: " + vehicleType +
+            ", Registration Fee: ₹" + registrationFee
+        );
+    }
+
+    // Class method
+    static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+
+    public static void main(String[] args) {
+
+        Vehicle v1 = new Vehicle("Aryaman", "Car");
+        Vehicle v2 = new Vehicle("Rohit", "Bike");
+
+        v1.displayVehicleDetails();
+        v2.displayVehicleDetails();
+
+        // Update registration fee for all vehicles
+        Vehicle.updateRegistrationFee(6500.0);
+
+        System.out.println("\nAfter updating registration fee:\n");
+
+        v1.displayVehicleDetails();
+        v2.displayVehicleDetails();
+    }
 }
