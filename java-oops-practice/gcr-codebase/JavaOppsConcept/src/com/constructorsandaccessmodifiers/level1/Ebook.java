13ea06321eaa1d1ca7c487b921350b2e0cde5a48
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-12-27 20:48:22 +0530

    [Aryaman]: added Book Library System program

--- java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/constructorsandaccessmodifiers/level1/Ebook.java
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/constructorsandaccessmodifiers/level1/Ebook.java
@@ -1,5 +1,36 @@
 package com.constructorsandaccessmodifiers.level1;
 
-public class Ebook {
+class EBook extends Books {
 
+    String fileFormat;
+
+    EBook(String ISBN, String title, String author, String fileFormat) {
+        super(ISBN, title, author);
+        this.fileFormat = fileFormat;
+    }
+
+    void displayDetails() {
+        System.out.println(
+            "ISBN: " + ISBN +            // public
+            ", Title: " + title +        // protected
+            ", Format: " + fileFormat +
+            ", Author: " + getAuthor()   // private via getter
+        );
+    }
+
+    public static void main(String[] args) {
+
+        EBook ebook = new EBook(
+            "978-0134685991",
+            "Effective Java",
+            "Joshua Bloch",
+            "PDF"
+        );
+
+        ebook.displayDetails();
+
+        // Modify private author using setter
+        ebook.setAuthor("J. Bloch");
+        System.out.println("Updated Author: " + ebook.getAuthor());
+    }
 }
