ac7fbe524de037328f1a5f1c570172a9cd750cd3
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-12-29 00:05:35 +0530

    [Aryaman]: added Rotate array by k position program

---
+++ leet-code-codebase/Array/RotateArray.java
@@ -0,0 +1,56 @@
+/* 189. Rotate Array */
+
+
+import java.util.Scanner;
+
+public class RotateArray {
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        // Input array elements
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // Input k
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        // Rotate array
+        rotate(nums, k);
+
+        // Output rotated array
+        System.out.println("Rotated array:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
