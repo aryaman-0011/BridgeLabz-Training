98872fa37fbdfd2a1e3accfac5a9643aa492753a
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-12-31 22:15:59 +0530

    [Aryaman]: added Vehicle Management System with Hybrid Inheritance program

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/inheritance/hybridinheritance/vehiclemanagement/ElectricVehicle.java
@@ -0,0 +1,12 @@
+package com.inheritance.hybridinheritance.vehiclemanagement;
+
+public class ElectricVehicle extends Vehicle {
+
+    ElectricVehicle(int maxSpeed, String model) {
+        super(maxSpeed, model);
+    }
+
+    void charge() {
+        System.out.println("Electric vehicle is charging");
+    }
+}
