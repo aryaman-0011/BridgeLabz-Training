f0b87e0d658b767a0842e345156f4a6d70822a82
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-05 11:31:27 +0530

    [Aryaman]: modified longestsonsecutivesequence

--- dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmapandfunctions/consecutivesequence/LongestConsecutiveSequence.java
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmapandfunctions/consecutivesequence/LongestConsecutiveSequence.java
@@ -1,5 +1,36 @@
 package com.hashmapandfunctions.consecutivesequence;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestConsecutiveSequence {
 
+    public static int longestConsecutive(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : nums) {
+
+            // Check if this is the start of a sequence
+            if (!set.contains(num - 1)) {
+
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longest = Math.max(longest, currentStreak);
+            }
+        }
+
+        return longest;
+    }
 }
