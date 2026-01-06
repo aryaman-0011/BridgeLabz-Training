a58789cd55aed9c555a2c089db8b893a5aea498c
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-06 22:33:28 +0530

    [Aryaman]: modified Merge Sort - Sort an Array of Book Prices program

--- dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/sorting/mergesort/MergeSort.java
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/sorting/mergesort/MergeSort.java
@@ -2,4 +2,66 @@ package com.sorting.mergesort;
 
 public class MergeSort {
 
+    // Public method to start merge sort
+    public static void sort(int[] prices) {
+        if (prices.length < 2) {
+            return;
+        }
+        mergeSort(prices, 0, prices.length - 1);
+    }
+
+    // Recursive merge sort
+    private static void mergeSort(int[] arr, int left, int right) {
+
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            // Sort left half
+            mergeSort(arr, left, mid);
+
+            // Sort right half
+            mergeSort(arr, mid + 1, right);
+
+            // Merge both halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Merge two sorted halves
+    private static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = arr[left + i];
+        }
+
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+
+        // Merge by comparison
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        // Copy remaining elements
+        while (i < n1) {
+            arr[k++] = leftArr[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = rightArr[j++];
+        }
+    }
 }
--- dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/sorting/mergesort/MergeSortMain.java
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/sorting/mergesort/MergeSortMain.java
@@ -1,5 +1,29 @@
 package com.sorting.mergesort;
 
+import java.util.Scanner;
+
 public class MergeSortMain {
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of books: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.println("Enter book prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        // Calling Merge Sort
+        MergeSort.sort(prices);
+
+        System.out.println("Sorted Book Prices (Ascending Order):");
+        for (int price : prices) {
+            System.out.print(price + " ");
+        }
+    }
 }
