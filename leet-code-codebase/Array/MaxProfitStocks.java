e3c9cb038a7130caadf13c0ff6518f43c9d27e16
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-07 00:18:16 +0530

    [Aryaman]: added max profit stocks program

---
+++ leet-code-codebase/Array/MaxProfitStocks.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class MaxProfitStocks {
+
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int profit = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int result = maxProfit(prices);
+        System.out.println("Maximum Profit: " + result);
+
+        sc.close();
+    }
+}
