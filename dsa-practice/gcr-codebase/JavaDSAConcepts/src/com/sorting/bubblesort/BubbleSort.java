b553a64b0e08bdf4bdaeb3cc231e7264a90b1288
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-06 22:28:58 +0530

    [Aryaman]: modified Bubble Sort - Sort Student Marks program

--- dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/sorting/bubblesort/BubbleSort.java
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/sorting/bubblesort/BubbleSort.java
@@ -2,4 +2,27 @@ package com.sorting.bubblesort;
 
 public class BubbleSort {
 
+    // Method to sort marks using Bubble Sort
+    public static void sort(int[] marks) {
+
+        int n = marks.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (marks[j] > marks[j + 1]) {
+                    int temp = marks[j];
+                    marks[j] = marks[j + 1];
+                    marks[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // Optimization: stop if already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
 }
--- dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/sorting/bubblesort/BubbleSortMain.java
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/sorting/bubblesort/BubbleSortMain.java
@@ -1,5 +1,29 @@
 package com.sorting.bubblesort;
 
+import java.util.Scanner;
+
 public class BubbleSortMain {
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[] marks = new int[n];
+
+        System.out.println("Enter student marks:");
+        for (int i = 0; i < n; i++) {
+            marks[i] = sc.nextInt();
+        }
+
+        // Calling Bubble Sort method
+        BubbleSort.sort(marks);
+
+        System.out.println("Sorted student marks (Ascending Order):");
+        for (int mark : marks) {
+            System.out.print(mark + " ");
+        }
+    }
 }
