0ca2f693b3a4edee957c9a699c0bc33ba32681c2
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-07 00:17:05 +0530

    [Aryaman]: added find peak element program

---
+++ leet-code-codebase/Array/FindPeakElement.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class FindPeakElement {
+
+    public static int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start; // index of peak element
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int peakIndex = findPeakElement(nums);
+
+        System.out.println("Peak element index: " + peakIndex);
+        System.out.println("Peak element value: " + nums[peakIndex]);
+
+        sc.close();
+    }
+}
