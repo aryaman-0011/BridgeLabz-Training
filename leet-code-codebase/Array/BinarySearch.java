829e0e720c93e4c94d713d847444f25880a34dfb
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-07 20:44:26 +0530

    [Aryaman]: added binary search program

---
+++ leet-code-codebase/Array/BinarySearch.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } 
+            else if (target > nums[mid]) {
+                start = mid + 1;
+            } 
+            else {
+                return mid; // target found
+            }
+        }
+        return -1; // target not found
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target value: ");
+        int target = sc.nextInt();
+
+        int result = search(nums, target);
+
+        if (result != -1) {
+            System.out.println("Target found at index: " + result);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        sc.close();
+    }
+}
