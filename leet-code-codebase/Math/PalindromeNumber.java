0fefb258ccc88215b34d18feadc5eeb8b7acbb6b
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-08 21:55:49 +0530

    [Aryaman]: added palindrome number program

---
+++ leet-code-codebase/Math/PalindromeNumber.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+    public static boolean isPalindrome(int x) {
+
+        // Negative numbers are not palindrome
+        if (x < 0) return false;
+
+        int original = x;
+        int reverse = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+
+        boolean result = isPalindrome(x);
+
+        if (result) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not a Palindrome Number");
+        }
+
+        sc.close();
+    }
+}
