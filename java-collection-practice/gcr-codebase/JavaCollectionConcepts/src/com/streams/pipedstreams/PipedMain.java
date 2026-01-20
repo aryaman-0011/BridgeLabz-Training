7507668550ef74a802171df171a6432fcf511b17
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-20 20:47:19 +0530

    [Aryaman]: modified Piped Streams - Inter-Thread Communication program

--- java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/streams/pipedstreams/PipedMain.java
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/streams/pipedstreams/PipedMain.java
@@ -1,5 +1,25 @@
 package com.streams.pipedstreams;
 
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
 public class PipedMain {
 
+    public static void main(String[] args) {
+
+        try {
+            PipedOutputStream pos = new PipedOutputStream();
+            PipedInputStream pis = new PipedInputStream(pos);
+
+            WriterThread writer = new WriterThread(pos);
+            ReaderThread reader = new ReaderThread(pis);
+
+            reader.start();
+            writer.start();
+
+        } catch (IOException e) {
+            System.out.println("Pipe connection error");
+        }
+    }
 }
--- java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/streams/pipedstreams/WriterThread.java
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/streams/pipedstreams/WriterThread.java
@@ -1,5 +1,26 @@
 package com.streams.pipedstreams;
 
-public class WriterThread {
+import java.io.IOException;
+import java.io.PipedOutputStream;
 
+public class WriterThread extends Thread {
+
+    private PipedOutputStream pos;
+
+    public WriterThread(PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String message = "Hello from Writer Thread";
+
+            pos.write(message.getBytes());
+            pos.close();
+
+        } catch (IOException e) {
+            System.out.println("Writer error");
+        }
+    }
 }
