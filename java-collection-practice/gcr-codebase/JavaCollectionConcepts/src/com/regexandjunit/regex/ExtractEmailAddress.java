0590edd180093dfd80aba9e3c0506c4b3d682a2c
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-24 11:30:34 +0530

    [Aryaman]: added Extract All Email Addresses from a Text program

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regexandjunit/regex/ExtractEmailAddress.java
@@ -0,0 +1,21 @@
+package com.regexandjunit.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractEmailAddress {
+
+    public static void main(String[] args) {
+
+        String text = "Contact us at support@example.com and info@company.org";
+
+        String regex = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
