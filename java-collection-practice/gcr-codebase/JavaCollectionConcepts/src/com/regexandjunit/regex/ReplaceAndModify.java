4c8460605761596272f2b3c5b1262853d28cf373
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-24 12:16:14 +0530

    [Aryaman]: added Replace Multiple Spaces with a Single Space program

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regexandjunit/regex/ReplaceAndModify.java
@@ -0,0 +1,13 @@
+package com.regexandjunit.regex;
+
+public class ReplaceAndModify {
+
+    public static void main(String[] args) {
+
+        String input = "This   is   an   example    with   multiple   spaces.";
+
+        String output = input.replaceAll("\\s+", " ");
+
+        System.out.println(output);
+    }
+}
