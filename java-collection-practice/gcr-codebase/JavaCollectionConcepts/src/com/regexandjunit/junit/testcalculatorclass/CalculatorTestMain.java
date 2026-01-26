d95d58aca2aeeaff24d834aa6c51428194c3cb2a
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-26 21:44:42 +0530

    [Aryaman]: added Basic JUnit Test: Testing a Calculator Class program

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regexandjunit/junit/testcalculatorclass/CalculatorTestMain.java
@@ -0,0 +1,37 @@
+package com.regexandjunit.junit.testcalculatorclass;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTestMain {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(10, calc.add(6, 4));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(2, calc.subtract(6, 4));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(24, calc.multiply(6, 4));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2, calc.divide(8, 4));
+    }
+
+    @Test
+    void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> {
+            calc.divide(10, 0);
+        });
+    }
+}
