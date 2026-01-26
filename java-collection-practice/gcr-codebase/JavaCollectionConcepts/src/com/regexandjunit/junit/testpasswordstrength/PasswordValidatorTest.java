39c191172544d38f8458ff387eed16c2f8d04d1d
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-26 23:53:19 +0530

    [Aryaman]: added Testing Password Strength Validator program

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regexandjunit/junit/testpasswordstrength/PasswordValidatorTest.java
@@ -0,0 +1,29 @@
+package com.regexandjunit.junit.testpasswordstrength;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    PasswordValidator validator = new PasswordValidator();
+
+    @Test
+    void testValidPassword() {
+        assertTrue(validator.isValid("JavaTest1"));
+    }
+
+    @Test
+    void testTooShortPassword() {
+        assertFalse(validator.isValid("J1ava"));
+    }
+
+    @Test
+    void testNoUppercase() {
+        assertFalse(validator.isValid("javatest1"));
+    }
+
+    @Test
+    void testNoDigit() {
+        assertFalse(validator.isValid("JavaTest"));
+    }
+}
