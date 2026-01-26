2e8e2a156637d9d49a93773d980455001a1ce7da
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-26 23:56:52 +0530

    [Aryaman]: added Testing Date Formatter program

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regexandjunit/junit/testdateformatter/DateFormatterTest.java
@@ -0,0 +1,33 @@
+package com.regexandjunit.junit.testdateformatter;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateFormatterTest {
+
+    DateFormatter formatter = new DateFormatter();
+
+    @Test
+    void testValidDate() {
+        assertEquals("26-01-2026", formatter.formatDate("2026-01-26"));
+    }
+
+    @Test
+    void testAnotherValidDate() {
+        assertEquals("01-12-2025", formatter.formatDate("2025-12-01"));
+    }
+
+    @Test
+    void testInvalidDateFormat() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            formatter.formatDate("26/01/2026");
+        });
+    }
+
+    @Test
+    void testInvalidDateValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            formatter.formatDate("2026-13-40");
+        });
+    }
+}
