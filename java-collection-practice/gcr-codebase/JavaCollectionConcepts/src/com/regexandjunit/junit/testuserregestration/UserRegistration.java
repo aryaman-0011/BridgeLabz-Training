21f4c8f990b23e8effa110ad96ac26168149fc6f
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-26 23:59:19 +0530

    [Aryaman]: added Testing User Registration program

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regexandjunit/junit/testuserregestration/UserRegistration.java
@@ -0,0 +1,21 @@
+package com.regexandjunit.junit.testuserregestration;
+
+public class UserRegistration {
+
+    public boolean registerUser(String username, String email, String password) {
+
+        if (username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("Invalid username");
+        }
+
+        if (email == null || !email.contains("@")) {
+            throw new IllegalArgumentException("Invalid email");
+        }
+
+        if (password == null || password.length() < 6) {
+            throw new IllegalArgumentException("Invalid password");
+        }
+
+        return true; // registration successful
+    }
+}
---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regexandjunit/junit/testuserregestration/UserRegistrationTest.java
@@ -0,0 +1,35 @@
+package com.regexandjunit.junit.testuserregestration;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRegistrationTest {
+
+    UserRegistration reg = new UserRegistration();
+
+    @Test
+    void testValidRegistration() {
+        assertTrue(reg.registerUser("Aryaman", "aryaman@mail.com", "pass123"));
+    }
+
+    @Test
+    void testInvalidUsername() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            reg.registerUser("", "user@mail.com", "pass123");
+        });
+    }
+
+    @Test
+    void testInvalidEmail() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            reg.registerUser("User", "usermail.com", "pass123");
+        });
+    }
+
+    @Test
+    void testInvalidPassword() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            reg.registerUser("User", "user@mail.com", "123");
+        });
+    }
+}
