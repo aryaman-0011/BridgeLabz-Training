478b49423295862d5ede9724af946c445f8c16fa
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-31 14:59:48 +0530

    [Aryaman]: modified Smart Vehicle Dashboard program

--- java8-programming-practice/gcr-codebase/Java8Concepts/src/com/functionalinterfaces/vehicledashboard/Vehicle.java
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/functionalinterfaces/vehicledashboard/Vehicle.java
@@ -2,4 +2,9 @@ package com.functionalinterfaces.vehicledashboard;
 
 public interface Vehicle {
 
+    void displaySpeed();
+
+    default void displayBattery() {
+        System.out.println("Battery info not available for this vehicle.");
+    }
 }
