dc070fa54a94b562165dbebb0425036b74e53cea
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-01-31 14:53:25 +0530

    [Aryaman]: added Smart Vehicle Dashboard program

---
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/functionalinterfaces/vehicledashboard/DashboardMain.java
@@ -0,0 +1,5 @@
+package com.functionalinterfaces.vehicledashboard;
+
+public class DashboardMain {
+
+}
---
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/functionalinterfaces/vehicledashboard/Vehicle.java
@@ -0,0 +1,5 @@
+package com.functionalinterfaces.vehicledashboard;
+
+public interface Vehicle {
+
+}
