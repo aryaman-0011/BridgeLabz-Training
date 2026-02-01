f5a4bbf9e956af20622bbb7508150f8d7da8b327
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-02-01 11:28:21 +0530

    [Aryaman]: added Banking / Finance program

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioBased/src/com/day_1/banking/BankService.java
@@ -0,0 +1,28 @@
+package com.day_1.banking;
+
+import java.util.function.*;
+
+public class BankService {
+
+    public static Predicate<Account> minimumBalanceRule =
+            acc -> acc.getBalance() >= 1000;
+
+    public static BiFunction<Account, Double, Double> calculateInterest =
+            (acc, rate) -> (acc.getBalance() * rate * acc.getYears()) / 100;
+
+    public static BiPredicate<Account, Double> validateWithdrawal =
+            (acc, amount) -> amount <= acc.getBalance();
+
+    public static Consumer<Account> printAccount =
+            acc -> System.out.println(acc);
+
+    public static BiFunction<Account, Account, String> compareBalance =
+            (a1, a2) -> {
+                if (a1.getBalance() > a2.getBalance())
+                    return a1.getAccountHolder() + " has higher balance.";
+                else if (a1.getBalance() < a2.getBalance())
+                    return a2.getAccountHolder() + " has higher balance.";
+                else
+                    return "Both accounts have equal balance.";
+            };
+}
