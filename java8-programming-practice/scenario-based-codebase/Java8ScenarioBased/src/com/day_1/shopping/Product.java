d34f05f87163cd393836488b4be7b7db0693badd
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-02-01 11:27:13 +0530

    [Aryaman]: added Shopping / E-Commerce program

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioBased/src/com/day_1/shopping/Product.java
@@ -0,0 +1,22 @@
+package com.day_1.shopping;
+
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getQuantity() { return quantity; }
+
+    @Override
+    public String toString() {
+        return name + " | Price: " + price + " | Qty: " + quantity;
+    }
+}
